e98f4d53048638f23dbd23a235a2e2e196dcebc7
romejg <justineguillermo00@gmail.com>
2023-01-31 13:40:08 +0800

    add detailed documentation on number graph

--- practice/1.py
+++ practice/1.py
@@ -1,17 +1,33 @@
+# Initialize an empty list to store the numbers
 nums=[]
+# Initialize a string variable
 n=''
+
+# Ask the user for the number of columns
 col=int(input("howmany columns: "))
+# Loop through the number of columns and ask for a number for each
 for i in range(col):
-    nums.append(int(input("enter a number: "))) ##transforms string input to int
+     #transforms string input to int
+    nums.append(int(input("enter a number: ")))
     
-max = max(nums)#gets the highest number in the array
+#gets the highest number in the array
+max = max(nums)
+
+#logic sa forloop na to is if yung nums[j] is < the max it will print a space
+#otherwise will print an asterisk
 for i in range(max):
-    for j in range(len(nums)):##logic sa forloop na to is if yung nums[j] is < the max it will print a space otherwise will print an asterisk
+    # Loop through the length of the list of numbers
+    for j in range(len(nums)):
+        # If the current number is less than the max, print a space
         if nums[j]<max:
-            print(f'{n:<3}',end='') #end='' so that walang new line na ma print
+            #end='' so that walang new line na ma print
+            print(f'{n:<3}',end='')
+        # If the current number is equal to or greater than the max, print an asterisk
         else:
-            print(f'*{n:<2}',end='')## the n:<2 is to format the spacing for the output
-    print()#so that it wil go to the next line :>
+            # the n:<2 is to format the spacing for the output
+            print(f'*{n:<2}',end='')
+    #so that it wil go to the next line :>
+    print()
     max-=1
 for i in nums:
     print(i,' ',end='')
